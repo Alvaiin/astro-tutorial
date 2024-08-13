95dcc3226f94ea895bb919def6a3f14b18a2a433
Alvaiin <alvain93@gmail.com>
2024-08-12 22:13:53 -0300

    fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://alvaiin.github.io',
+    site: 'https://alvaiin.github.io/astro-tutorial',
   });
