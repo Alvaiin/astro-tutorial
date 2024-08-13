e083157cc527bcc6c22bb6923fdae2b2d3e98815
Alvaiin <alvain93@gmail.com>
2024-08-12 22:31:09 -0300

    add base name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://alvaiin.github.io/astro-tutorial',
+    site: 'https://alvaiin.github.io',
+    base: '/astro-tutorial',
   });
